867f8ab8165d49e1bd9accdbc64fc1b30d5e1192
nextbs <nextbs100000@gmail.com>
2025-01-16 12:06:00 -0800

    enhanced ESLint configuration

--- eslintrc.js
+++ eslintrc.js
@@ -14,8 +14,56 @@
  */
 
 module.exports = {
-  extends: ['next/core-web-vitals'],
+  extends: [
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended'
+  ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  root: true
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'react-hooks'
+  ],
+  root: true,
+  rules: {
+    // TypeScript specific rules
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['warn', { 
+      argsIgnorePattern: '^_',
+      varsIgnorePattern: '^_'
+    }],
+
+    // React specific rules
+    'react/react-in-jsx-scope': 'off', // Not needed in Next.js
+    'react/prop-types': 'off', // We use TypeScript for props validation
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+
+    // General rules
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'eqeqeq': ['error', 'always'],
+    'no-unused-vars': 'off' // Using TypeScript's no-unused-vars instead
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  ignorePatterns: [
+    'node_modules/',
+    '.next/',
+    'out/',
+    'build/',
+    '*.config.js'
+  ]
 } 
